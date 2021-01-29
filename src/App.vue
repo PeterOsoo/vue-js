9a3aa861b89043c2427370d15e692261dbb95761
PeterOsoo <posoo617@gmail.com>
2021-01-29 16:03:48 +0300

    Binding Classes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
+  <!-- Binding Text -->
+  <h1>Binding Text</h1>
   <div>{{greet}} {{name}}</div>
   <div v-text="channel"></div>
 
+  <!-- Binding HTML -->
+  <h1>Binding HTML</h1>
   <div v-text="detail"></div>
   <div v-html="detail"></div>
 
+  <!-- Binding Attributes -->
+  <h1>Binding Attributes</h1>
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+
+  <!-- Binding Classes -->
+  <h1>Binding Classes</h1>
+  <h2 class="underline">Underline</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? Movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+
+   <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut,
+    }"
+  >
+    Object Conditional Movie
+  </h2>
+
 </template>
 
 <script>
@@ -20,7 +49,10 @@ export default {
       channel: "Ondiek",
       detail: "<b> blacklist </b>",
       headingId: "Heading",
-      isDisabled: true
+      isDisabled: true,
+      status: "success",
+      isPromoted: true,
+      isSoldOut: true
     }
   }  
 }
@@ -29,5 +61,23 @@ export default {
 <style>
 #app {
   text-align: center;
+}
+
+h1 {
+  color: darkblue;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+.sold-out {
+  color: red;
+}
+.new {
+  color: olivedrab;
 }
 </style>
