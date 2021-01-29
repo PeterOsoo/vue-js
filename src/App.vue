95d4fe77e2519d48d51c95fdd4f7a898b4ed2daa
PeterOsoo <posoo617@gmail.com>
2021-01-29 15:34:55 +0300

    Binding to Attributes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div>{{greet}} {{name}}</div>
   <div v-text="channel"></div>
+
   <div v-text="detail"></div>
   <div v-html="detail"></div>
+
+  <h2 v-bind:id="headingId">Heading</h2>
 </template>
 
 <script>
@@ -14,7 +17,8 @@ export default {
       greet: "Hello",
       name: "Rateng",
       channel: "Ondiek",
-      detail: "<b> blacklist </b>"
+      detail: "<b> blacklist </b>",
+      headingId: "Heading"
     }
   }  
 }
