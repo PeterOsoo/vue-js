fd3aa74e93547717747b37401e63f76994121633
peterosoo <posoo617@gmail.com>
2021-02-02 12:35:41 +0300

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -119,6 +119,7 @@
   </div>
 
   <!-- Form Handling -->
+  <h1>Form Handling</h1>
   <div>
     <pre>
       {{ JSON.stringify(formValues, null, 2) }}
@@ -238,9 +239,38 @@
   <p>Key</p>
 
   <!-- Bonus Directives -->
+  <h1>Bonus Directives</h1>
   <h2 v-once>{{ name }}</h2>
   <button @click="name = 'Jakablak'">Change name</button>
   <h2 v-pre>{{ name }}</h2>
+
+  <!-- Computed Properties -->
+  <h1>Computed Properties</h1>
+  <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
+  <h2>Fullname - {{ fullName }}</h2>
+
+  <button @click="changeFullName">Change fullname</button>
+
+  <h2>
+    Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
+  </h2>
+  <h2>Computed Total - {{ total }}</h2>
+  <h2>Method Total - {{ getTotal() }}</h2>
+  <button @click="items.push({ id: 4, title: 'Keyboard', price: 50 })">
+    Add item
+  </button>
+  <div>
+    <input type="text" v-model="country" class="input-country" />
+  </div>
+
+  <!-- conditional list rendering  -->
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.id === 1">{{ item.title }} - {{ item.price }}</h2>
+  </template>
+
+  <h2 v-for="item in expensiveItems" :key="item.id">
+    {{ item.title }} - {{ item.price }}
+  </h2>
 </template>
 
 <script>
@@ -315,6 +345,31 @@ export default {
         yearsOfExperience: "",
         age: null,
       },
+      firstName: "Bruce",
+      lastName: "Wayne",
+      items: [
+        {
+          id: 1,
+          title: "Watch",
+          price: 100,
+        },
+        {
+          id: 2,
+          title: "Phone",
+          price: 200,
+        },
+        {
+          id: 3,
+          title: "Laptop",
+          price: 300,
+        },
+        {
+          id: 4,
+          title: "TV",
+          price: 400,
+        },
+      ],
+      country: "",
     };
   },
   methods: {
@@ -338,6 +393,38 @@ export default {
     },
     submitForm() {
       console.log("form values", this.formValues);
+    },
+    getTotal() {
+      console.log("getTotal method");
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
+    changeFullName() {
+      this.fullName = "Ondiek Japap";
+    },
+  },
+  computed: {
+    fullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(value) {
+        const names = value.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[1];
+      },
+    },
+    total() {
+      console.log("total computed property");
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
+    expensiveItems() {
+      return this.items.filter((item) => item.price > 100);
     },
   },
 };
