25cd3914e6f13f7caea73a793a785d163d598406
peterosoo <posoo617@gmail.com>
2021-02-03 10:47:04 +0300

    Prop Types and Validations

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,17 +1,22 @@
 <template>
+  <h1>Props</h1>
   <Greet name="japap" heroName="Super Man" />
   <Greet name="ondiek" heroName="Batman" />
   <Greet name="jaground" heroName="Wonder Man" />
   <Greet :name="name" :heroName="channel" />
+
+  <Article title="New Article by Ondiek" :likes="50" :isPublished="true" />
 </template>
 
 <script>
 import Greet from "./components/Greet.vue";
+import Article from "./components/Article.vue";
 
 export default {
   name: "App",
   components: {
     Greet,
+    Article,
   },
   data() {
     return {
