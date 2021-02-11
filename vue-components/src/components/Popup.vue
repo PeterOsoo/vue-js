d9ac5530ebd8e89396fd2dba8e33af71da888e73
peterosoo <posoo617@gmail.com>
2021-02-11 13:00:34 +0300

    emit data from input

--- vue-components/src/components/Popup.vue
+++ vue-components/src/components/Popup.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h3>this is popup</h3>
-    <button @click="$emit('close', 'Rateng')">close popup</button>
+    <input type="text" v-model="name" />
+    <button @click="$emit('close', name)">close popup</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Popup",
+  data() {
+    return {
+      name: "",
+    };
+  },
   emits: ["close"],
 };
 </script>
